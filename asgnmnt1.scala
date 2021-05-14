707cc46b2a33285d1ac38bb6ca07807350ffd009
SumuduPerera98 <sharshiperera20@gmail.com>
2021-05-15 02:40:29 +0530

    adding scala files

---
+++ asgnmnt1.scala
@@ -0,0 +1,14 @@
+object asgnmnt1 extends App{
+  def convert(x:Double)=x*1.8+32;
+  println (convert(35));
+  
+  def volume(y:Double)=(((4/3.0)*3.14)*y*y*y);
+  println (volume(5));
+
+  def total(z:Int)=(50*(24.95*0.6+3)+(z-50)*(24.95*0.6+0.75));
+  println (total(60));
+}
+
+
+
+
